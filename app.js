a4b9be6e8223b0c82d767f92dac0a3331b8de976
nicholas <nickalden@nicholass-MacBook-Pro.local>
2024-02-02 10:56:06 -0800

    added cors

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import router from "./routes/index.js"
 import bodyParser from "body-parser";
 import express from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
 dotenv.config();
 
 const port =process.env.PORT || 3000;
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
