b7b7da1c66b8c8f4e13294e632b186ee81984e98
nicholas <nickalden@nicholass-MacBook-Pro.local>
2024-02-08 16:38:48 -0800

    bufix

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ import UsersController from '../controllers/usersController.js';
 const router = express.Router();
 
 router.get('/', UsersController.getUser);
-router.post('/', UsersController.postUser);
+router.post('/', UsersController.updateUser);
+router.get('/:id', UsersController.getUserProfile);
 
 export default router;
